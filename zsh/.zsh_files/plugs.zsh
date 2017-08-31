195ad308bc60bfc7c43aa9c578b7062301bfb116
Mathias <mathiashls@protonmail.com>
2017-08-31 00:37:12 -0300

    Changed zsh theme lol

--- zsh/.zsh_files/plugs.zsh
+++ zsh/.zsh_files/plugs.zsh
@@ -5,20 +5,17 @@ fi
 source ~/.zplug/init.zsh
 
 zplug "mafredri/zsh-async"
-
 zplug "zsh-users/zsh-completions"
 zplug "tj/git-extras", as:plugin, use:"etc/*.zsh"
-
 zplug "sapegin/shipit", as:command, use:bin/shipit
 zplug "unixorn/git-extra-commands"
-
 zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
-
 zplug "chriskempson/base16-shell", as:plugin, use:base16-shell.plugin.zsh
 
-# zplug "sindresorhus/pure" # branch and enviroment
-zplug "yardnsm/blox-zsh-theme" # branch and commit
+# Themes:
+zplug "sindresorhus/pure" # branch and enviroment
+# zplug "yardnsm/blox-zsh-theme" # branch and commit
 
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then

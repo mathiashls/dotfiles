c60bbc0c60c59aad1c931021398ceded6f1c1b7d
Mathias Hillesheim <mathias.hillesheim@byne.com.br>
2017-11-22 16:39:18 -0200

    New alias and expoert for better workflow

--- zsh/.zsh_files/alias.zsh
+++ zsh/.zsh_files/alias.zsh
@@ -1,21 +1,16 @@
 # Directories:
-alias codir='cd /Users/mathias/Documents/code/'
-alias bdir='cd /Users/mathias/Documents/code/BYNE'
-alias ptc='cd /Users/mathias/Documents/code/IFSC/PTC'
-alias pji='cd /Users/mathias/Documents/code/IFSC/PJI3'
-alias ste='cd /Users/mathias/Documents/code/IFSC/STE'
-alias ifsc='cd /Users/mathias/Documents/code/IFSC'
+alias bdir='cd /Users/mathias/Documents/BYNE'
+alias ste='cd /Users/mathias/Documents/IFSC/8FASE/STE'
+alias ste='cd /Users/mathias/Documents/IFSC/8FASE/PTC'
 alias .files='cd ~/dotfiles'
-
-# Virtual enviroments:
-alias paleale='. ~/.scripts/paleale_virtualenv.sh'
+alias paleale='workon paleale'
 
 # Programs:
 alias vim='nvim'
 alias v='nvim'
 alias la='ls -lAhG'
-alias byne='connect_console.sh'
-alias upserver='update_server.sh'
-alias upsour='update_console.sh'
-alias upconsole='update_console.sh'
-alias bwatch='watch_konf.sh'
+alias byne='sh connect_to_remote.sh'
+alias upsv='sh upserver.sh'
+alias upserver='sh upserver.sh'
+alias upconsole='sh upconsole.sh'
+alias upweb='sh upweb.sh'
--- zsh/.zsh_files/export.zsh
+++ zsh/.zsh_files/export.zsh
@@ -1,5 +1,10 @@
-export EDITOR="vim"
+export EDITOR="nvim"
 export LC_ALL="en_US.UTF-8"
 export TERM=xterm-256color
-export PATH=/usr/local/bin:/Users/mathias/.scripts:$PATH
+export PATH=/usr/local/bin:/Users/mathias/Documents/scripts:$PATH
 export BYNEPATH=/Users/mathias/Documents/code/BYNE
+
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python2.7
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Devel
+source /usr/local/bin/virtualenvwrapper.sh

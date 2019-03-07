ba872ce773e71251b83dc1e68f0740cad79653ed
Mathias Hillesheim <mathiashls@protonmail.com>
2019-03-07 11:21:14 -0300

    Changes virtualenvwrapper python version to python3

--- zsh/.zsh_files/export.zsh
+++ zsh/.zsh_files/export.zsh
@@ -4,7 +4,7 @@ export TERM=xterm-256color
 export PATH=/usr/local/bin:/Users/mathias/code/scripts:$PATH
 export BYNEPATH=/Users/mathias/Documents/code/BYNE
 
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python2.7
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/Devel
 source /usr/local/bin/virtualenvwrapper.sh
